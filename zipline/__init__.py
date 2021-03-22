e56af550a4fe90229e10a66aadb74722b432ad31
Shlomi Kushchi <shlomik@gmail.com>
2021-03-22 17:24:19 +0200

    fix:: h5py ImportError: DLL load failed: The specified procedure could not be found.
    something gets fucked when imort zipline then h5py. probably order of loaded modules in py37 x64
    this fixes it by importing h5py first

--- zipline/__init__.py
+++ zipline/__init__.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import h5py
 from distutils.version import StrictVersion
 import os
 import numpy as np
